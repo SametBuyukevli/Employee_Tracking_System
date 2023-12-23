47bce4764a259cb60005b5d2e70b2100039e0c38
Sabet Buyukevli <sametertugrulbuyukevli@gmail.com>
2023-12-23 17:01:29 +0300

    update 0.2

--- data/db.js
+++ data/db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2/promise");
 // MySQL bağlantı havuzunu oluştur
 const pool = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "samet123",
-  database: "personnel_tracking_system",
+  user: "",
+  password: "",
+  database: "",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
